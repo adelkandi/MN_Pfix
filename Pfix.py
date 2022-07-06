cf7f5ede3243f935c0526fe110d7df0b9b12ef9c
Adel Kandi <kandiadel58@gmail.com>
2022-07-06 11:33:54 +0100

    New Update

--- Pfix.py
+++ Pfix.py
@@ -3,7 +3,7 @@ from pdb import line_prefix
 import numpy as np 
 from matplotlib import pyplot as plt
 import matplotlib as plot 
-
+from math import * 
 
 
 def f(x):
@@ -25,10 +25,25 @@ print(i) # print number of iteration
 
 # plot the functions graph :
 x = np.linspace(1,1,100)
-y = x**3 - (11/6)*(x**2)+x-1/6 # The function we will plot 
-plt.subplot(3,1,1)
-plt.plot(x ,y)
-plt.xlabel("axe x")
+y = x**3 - (11/6)*(x**2)+x-1/6 # The function f(x) we will plot 
+plt.subplot(1,1,1)
+plt.plot(x ,y)            # Plot the function graph 
+plt.xlabel("axe x")       
+plt.ylabel("axe y")
+
+# Plot the g(x) Function Graph 
+plt.subplot(2,1)
+xg = np.linspace(1,1,100)
+yg =  0  # init 0
+plt.plot(xg ,yg)            # Plot the function graph 
+plt.xlabel("axe x")       
+plt.ylabel("axe y")
+
+# Plot the g'(x) Graph 
+xg1 = np.linspace(1,1,100)
+yg1 =  0    # init 0
+plt.plot(xg1 ,yg1)            # Plot the function graph 
+plt.xlabel("axe x")       
 plt.ylabel("axe y")
 plt.show()
 
