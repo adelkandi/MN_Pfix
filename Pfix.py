bbe259dc07870410afdf2ffbd8c31fbde40c610a
Adel Kandi <kandiadel58@gmail.com>
2022-07-08 12:28:16 +0100

    New Update

--- Pfix.py
+++ Pfix.py
@@ -1,5 +1,5 @@
 
-from pdb import line_prefix
+
 import numpy as np 
 from matplotlib import pyplot as plt
 import matplotlib as plot 
@@ -12,16 +12,19 @@ def f(x):
 def g(x):
     3*x**2 - (11/6)*2*x + 1 # g(x) the derivative of the f function 
 
-
-x = 0 
+i = 1 # init the iteration to 1
+x = 0
 x1 = None   #Initialize x1 to none no value 
-eps = 10**-(int(input("Give the Epselon power:10**-")))
-while abs(x1-x) > eps:
-    x1 = g(x)       # change the value from x1 to g(x ) untile find out the racine of the function f(x)
-    i = i+1
+eps = float(input("The Error Value:"))
+def PFix(x,eps):
+    while abs(x1-x) > eps:
+        x1 = g(x)       # change the value from x1 to g(x ) untile find out the racine of the function f(x)
+        i = i+1
     
-print(x1)
-print(i) # print number of iteration  
+# New update :
+result = PFix(x,eps) # 
+print("The Solution of the function is: ",result)
+print("The Nuber of iteration is: ",i) # print number of iteration  
 
 # plot the functions graph :
 x = np.linspace(1,1,100)
@@ -34,14 +37,14 @@ plt.ylabel("axe y")
 # Plot the g(x) Function Graph 
 plt.subplot(2,1)
 xg = np.linspace(1,1,100)
-yg =  0  # init 0
+yg =  3*x**2 - (11/6)*2*x + 1  # g(x) Function 
 plt.plot(xg ,yg)            # Plot the function graph 
 plt.xlabel("axe x")       
 plt.ylabel("axe y")
 
 # Plot the g'(x) Graph 
 xg1 = np.linspace(1,1,100)
-yg1 =  0    # init 0
+yg1 =  6*x - (11/6)*6    # The g'(x) Function 
 plt.plot(xg1 ,yg1)            # Plot the function graph 
 plt.xlabel("axe x")       
 plt.ylabel("axe y")
